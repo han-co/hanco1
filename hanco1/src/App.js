d792ff0fc738458c282dabb5962ffd7f64cb5954
Cody Gonzalez <gonzalec1721@gmail.com>
2021-09-18 14:05:52 -0400

    Clean App.js

--- hanco1/src/App.js
+++ hanco1/src/App.js
@@ -3,21 +3,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          <h1>hello</h1>
-          <h1>Hannah Time!!</h1>
-        </a>
-      </header>
+      Learn React
+      <h1>hello</h1>
+      <h1>Hannah Time!!</h1>
     </div>
   );
 }
